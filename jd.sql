4436cfebcf80a0c9804d85f58a412f18d5e97483
linyifan <php7@php.net>
2019-09-15 22:16:34 +0800

    perfect project in 9.15

--- jd.sql
+++ jd.sql
@@ -6,44 +6,58 @@ USE jd;
 /**商品列表**/
 CREATE TABLE jd_product(
   lid INT PRIMARY KEY AUTO_INCREMENT,
-  smproimg VARCHAR(128),
-  lgproimg VARCHAR(128),
-  typeimg VARCHAR(128),#京东精选，自营图片
-  price DECIMAL(10,2),
-  vipprice DECIMAL(10,2),
-  buytype VARCHAR(10), #闪购，满减文字
-  details VARCHAR(128),
-  title VARCHAR(64)
+  smproimg VARCHAR
+(128),
+  lgproimg VARCHAR
+(128),
+  typeimg VARCHAR
+(128),#京东精选，自营图片
+  price DECIMAL
+(10,2),
+  vipprice DECIMAL
+(10,2),
+  buytype VARCHAR
+(10), #闪购，满减文字
+  details VARCHAR
+(128),
+  title VARCHAR
+(64)
 );
 
 /**轮播图片**/
 CREATE TABLE jd_carousel(
   cid INT PRIMARY KEY AUTO_INCREMENT,
-  cimg VARCHAR(128)             #轮播图路径
-); 
+  cimg VARCHAR
+(128)             #轮播图路径
+);
 
 /**用户信息**/
 CREATE TABLE jd_login(
   id INT PRIMARY KEY AUTO_INCREMENT,
-  uname VARCHAR(16),
-  upwd  VARCHAR(32)
+  uname VARCHAR
+(16),
+  upwd  VARCHAR
+(32)
 );
 
 /**京东秒杀**/
 CREATE TABLE jd_miaosha(
   mid INT PRIMARY KEY AUTO_INCREMENT,
-  mimg VARCHAR(128),
-  nowprice DECIMAL(10,2),
-  oldprice DECIMAL(10,2)
+  mimg VARCHAR
+(128),
+  nowprice INT,
+  oldprice INT
 );
 
 /**购物车条目**/
 CREATE TABLE jd_cart(
-  id     INT PRIMARY KEY AUTO_INCREMENT,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   lid    INT,
-  price  DECIMAL(10,2),
+  price  DECIMAL
+(10,2),
   count  INT,
-  lname  VARCHAR(255),
+  lname  VARCHAR
+(255),
   uid    INT
 );
 
@@ -51,18 +65,37 @@ CREATE TABLE jd_cart(
 /******数据导入******/
 /*******************/
 /**商品列表**/
-INSERT INTO jd_product VALUES(null,'../assets/wntj/1_sm.jpg','../assets/wntj/1_lg.jpg','../assets/zy.png',3988.00,3888.00,'闪购','【京东旗舰店+华为官方直供：全国联保+多重好礼(可联系客服折现)】【赠：10000毫安移动电源+真无线双耳蓝牙耳机+高品质运动手环等好礼】华为P30pro','华为P30手机 【白条6期免息0首付+20天价保+现货当天发+1年碎屏险】 天空之境 全网通 8G+128G(6期免息)'),
-(null,'../assets/wntj/2_sm.jpg','../assets/wntj/2_lg.jpg','../assets/jdjx.png',1999.00,1899.00,'新品','限时特惠300元！成交价1999！屏下摄像头，骁龙710，后置三摄！Note10系列正式预售！预定即享白条6期免息》','三星 Galaxy A8s 全面屏手机 6GB+128GB 极光黑 全网通 双卡双待 4G手机 自营'),
-(null,'../assets/wntj/3_sm.jpg','../assets/wntj/3_lg.jpg','../assets/zy.png',988.00,978.00,'满减','【电视购物同款】台上式，体积小，不用电，无废水，大流量，保留水中有益矿物质，支持免费上门安装！','乔治史帝夫净水器 家用台上式净水机器史蒂夫厨房自来水龙头过滤器 不锈钢正品SAT900型直饮水机'),
-(null,'../assets/wntj/4_sm.jpg','../assets/wntj/4_lg.jpg','../assets/jdjx.png',2399.00,2299.00,null,'限时特惠200元！成交价2399！屏下摄像头，骁龙710，后置三摄！Note10系列正式预售！预定即享白条6期免息》','三星 Galaxy A8s 全面屏手机 8GB+128GB 极光黑 全网通 双卡双待 4G手机 自营'),
-(null,'../assets/wntj/5_sm.jpg','../assets/wntj/5_lg.jpg','../assets/zy.png',3199.00,3099.00,'秒杀','【咨询客服优惠100元】【白条6期免息】【下单送】华为原装智能手环+钢化膜+运动蓝牙耳机+充电宝+一拖三数据线+碎屏险+运费险。华为P30','HUAWEI华为Mate20 手机【6期免息】 亮黑色 全网通(6GB 64GB) 送华为智能手环/碎屏险'),
-(null,'../assets/wntj/6_sm.jpg','../assets/wntj/6_lg.jpg','../assets/jdjx.png',4980.00,4880.00,'秒杀','【新品上市，5芯6级过滤，自来水过滤直饮，水质监测功能，净水自动加热一体，全国免费上门安装','八分（bafeen）净水器家用直饮加热一体机厨房壁挂纯水机ro反渗透台式冷热净水机 BF-S20D-100G'),
-(null,'../assets/wntj/7_sm.jpg','../assets/wntj/7_lg.jpg','../assets/zy.png',1999.00,1899.00,'满减','【7.26京东超级新品日】旗舰单品限量到货详情猛戳','【预售版】华为平板 M6 8.4英寸麒麟980影音娱乐平板电脑4GB+64GB WiFi（香槟金）'),
-(null,'../assets/wntj/8_sm.jpg','../assets/wntj/8_lg.jpg','../assets/zy.png',20800.00,19800.00,'满减','拥有GPR150限量版拉风从此刻开始！温馨提示：选择定金购买提车时需要到就近线下门店或者线上支付尾款，谢谢！','阿普利亚摩托车GPR150赛道跑车限量版 街车 国四 限量版 银色 全款');
+INSERT INTO jd_product
+VALUES(null, '../assets/wntj/1_sm.jpg', '../assets/wntj/1_lg.jpg', '../assets/zy.png', 3988.00, 3888.00, '闪购', '【京东旗舰店+华为官方直供：全国联保+多重好礼(可联系客服折现)】【赠：10000毫安移动电源+真无线双耳蓝牙耳机+高品质运动手环等好礼】华为P30pro', '华为P30手机 【白条6期免息0首付+20天价保+现货当天发+1年碎屏险】 天空之境 全网通 8G+128G(6期免息)'),
+  (null, '../assets/wntj/2_sm.jpg', '../assets/wntj/2_lg.jpg', '../assets/jdjx.png', 1999.00, 1899.00, '新品', '限时特惠300元！成交价1999！屏下摄像头，骁龙710，后置三摄！Note10系列正式预售！预定即享白条6期免息》', '三星 Galaxy A8s 全面屏手机 6GB+128GB 极光黑 全网通 双卡双待 4G手机 自营'),
+  (null, '../assets/wntj/3_sm.jpg', '../assets/wntj/3_lg.jpg', '../assets/zy.png', 988.00, 978.00, '满减', '【电视购物同款】台上式，体积小，不用电，无废水，大流量，保留水中有益矿物质，支持免费上门安装！', '乔治史帝夫净水器 家用台上式净水机器史蒂夫厨房自来水龙头过滤器 不锈钢正品SAT900型直饮水机'),
+  (null, '../assets/wntj/4_sm.jpg', '../assets/wntj/4_lg.jpg', '../assets/jdjx.png', 2399.00, 2299.00, null, '限时特惠200元！成交价2399！屏下摄像头，骁龙710，后置三摄！Note10系列正式预售！预定即享白条6期免息》', '三星 Galaxy A8s 全面屏手机 8GB+128GB 极光黑 全网通 双卡双待 4G手机 自营'),
+  (null, '../assets/wntj/5_sm.jpg', '../assets/wntj/5_lg.jpg', '../assets/zy.png', 3199.00, 3099.00, '秒杀', '【咨询客服优惠100元】【白条6期免息】【下单送】华为原装智能手环+钢化膜+运动蓝牙耳机+充电宝+一拖三数据线+碎屏险+运费险。华为P30', 'HUAWEI华为Mate20 手机【6期免息】 亮黑色 全网通(6GB 64GB) 送华为智能手环/碎屏险'),
+  (null, '../assets/wntj/6_sm.jpg', '../assets/wntj/6_lg.jpg', '../assets/jdjx.png', 4980.00, 4880.00, '秒杀', '【新品上市，5芯6级过滤，自来水过滤直饮，水质监测功能，净水自动加热一体，全国免费上门安装', '八分（bafeen）净水器家用直饮加热一体机厨房壁挂纯水机ro反渗透台式冷热净水机 BF-S20D-100G'),
+  (null, '../assets/wntj/7_sm.jpg', '../assets/wntj/7_lg.jpg', '../assets/zy.png', 1999.00, 1899.00, '满减', '【7.26京东超级新品日】旗舰单品限量到货详情猛戳', '【预售版】华为平板 M6 8.4英寸麒麟980影音娱乐平板电脑4GB+64GB WiFi（香槟金）'),
+  (null, '../assets/wntj/8_sm.jpg', '../assets/wntj/8_lg.jpg', '../assets/zy.png', 20800.00, 19800.00, '满减', '拥有GPR150限量版拉风从此刻开始！温馨提示：选择定金购买提车时需要到就近线下门店或者线上支付尾款，谢谢！', '阿普利亚摩托车GPR150赛道跑车限量版 街车 国四 限量版 银色 全款');
 /**用户信息**/
-INSERT INTO jd_login VALUES(null,'tom',md5('123'));
-INSERT INTO jd_login VALUES(null,'jerry',md5('123'));
+INSERT INTO jd_login
+VALUES(null, 'tom', md5('123'));
+INSERT INTO jd_login
+VALUES(null, 'jerry', md5('123'));
 /**轮播图片**/
-INSERT INTO jd_carousel VALUES(null,'../assets/lunbo/cal-1.jpg'),(null,'../assets/lunbo/cal-2.jpg'),(null,'../assets/lunbo/cal-3.jpg'),(null,'../assets/lunbo/cal-4.jpg'),(null,'../assets/lunbo/cal-5.jpg'),(null,'../assets/lunbo/cal-6.jpg'),(null,'../assets/lunbo/cal-7.jpg'),(null,'../assets/lunbo/cal-8.jpg');
+INSERT INTO jd_carousel
+VALUES(null, '../assets/lunbo/cal-1.jpg'),
+  (null, '../assets/lunbo/cal-2.jpg'),
+  (null, '../assets/lunbo/cal-3.jpg'),
+  (null, '../assets/lunbo/cal-4.jpg'),
+  (null, '../assets/lunbo/cal-5.jpg'),
+  (null, '../assets/lunbo/cal-6.jpg'),
+  (null, '../assets/lunbo/cal-7.jpg'),
+  (null, '../assets/lunbo/cal-8.jpg');
 /**京东秒杀**/
-INSERT INTO jd_miaosha VALUES(null,'../assets/miaosha/ms1.jpg',299.00,399.00),(null,'../assets/miaosha/ms2.jpg',1299.00,1399.00)
+INSERT INTO jd_miaosha
+VALUES(null, '../assets/miaosha/ms1.jpg', 279, 299),
+  (null, '../assets/miaosha/ms2.jpg', 1279, 1399),
+  (null, '../assets/miaosha/ms3.jpg', 599, 466),
+  (null, '../assets/miaosha/ms4.jpg', 59, 100),
+  (null, '../assets/miaosha/ms5.jpg', 79, 89), 
+  (null, '../assets/miaosha/ms6.png', 59, 100),
+  (null, '../assets/miaosha/ms7.jpg', 69, 99),
+  (null, '../assets/miaosha/ms8.png', 366,315);
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,109 +1,149 @@
 <template>
   <div>
-    <!-- 上方搜索框 -->
-    <div class="top_input">
-      <span></span>
-      <div class="input">
-        <a href="javascript:;">dell显示器</a>
+    <!-- app推广 -->
+    <div class="app_tg">
+      <div>
+        <img src="../assets/off.png" alt="">
+        <img src="../assets/jd_app.png" alt="">
+        <div>
+          <span>打开京东App购物</span>
+          <span>新人领188元红包</span>
+        </div>
       </div>
-      <a href="javascript:;">登录</a> 
-    </div>
+      <div><a href="javascript:;">立即打开</a></div>
+    </div> 
     <!-- 商品导航列表 -->
     <div class="container">
-      <!-- 轮播图 -->
-      <carousel></carousel>
-      <!-- 中部 -->
-      <div class="list">
-        <div class="list-item">
-          <img src="../assets/icon/016033c7ef3e0c6c.png">
-          <span>领券</span>
+        <!-- 上方搜索框 -->
+      <div class="top_input">
+        <span></span>
+        <div class="input">
+          <a href="javascript:;">dell显示器</a>
         </div>
-        <div class="list-item">
-          <img src="../assets/icon/01e0e818a7505267.png">
-          <span>京东超市</span>
-        </div>
-        <div class="list-item">
-          <img src="../assets/icon/28cda0a571b4a576.png">
-          <span>数码电器</span>
-        </div>
-        <div class="list-item">
-          <img src="../assets/icon/4792ec307a853e9f.png">
-          <span>PLUS会员</span>
-        </div>
-        <div class="list-item">
-          <img src="../assets/icon/5ae01befN2494769f.png">
-          <span>京东到家</span>
-        </div>
+        <a href="javascript:;">登录</a> 
       </div>
-      <div class="list">
-        <div class="list-item">
-          <img src="../assets/icon/5ad87bf0N66c5db7c.png">
-          <span>京东生鲜</span>
-        </div>
-        <div class="list-item">
-          <img src="../assets/icon/5b03b74eN3541598d.png">
-          <span>充值缴费</span>
-        </div>
-         <div class="list-item">
-          <img src="../assets/icon/5b03b779N5c0b196f.png">
-          <span>赚钱</span>
-        </div>
-         <div class="list-item">
-          <img src="../assets/icon/5b03fae3N67f78fe3.png">
-          <span>9.9元拼</span>
-        </div>
-         <div class="list-item">
-          <img src="../assets/icon/65c0cdc1362635fc.png">
-          <span>京东服饰</span>
-        </div>
-      </div>
-      <!-- 下部 -->
-      <div class="zhongqiu">
-        <div>
-          <a href="javascript:;"><img src="../assets/1.jpg"></a>
-        </div>
-        <div>
-          <a href="javascript:;"><img src="../assets/2.jpg"></a>
-        </div>
-        <div>
-          <a href="javascript:;"><img src="../assets/3.jpg"></a>
-        </div>
-      </div>
-      <!-- 京东秒杀 -->
-      <div class="jdms">
-        <div>
-          <div>
-            <span></span>
-            <span>18点场</span>
-            <div class="djs">
-              <span class="number">00</span>
-              <span>:</span>
-              <span class="number">42</span>
-              <span>:</span>
-              <span class="number">50</span>
-            </div>
+      <!-- 主要商品 -->
+      <div>
+        <!-- 轮播图 -->
+        <carousel></carousel>
+        <!-- 中部 -->
+        <div class="list">
+          <div class="list-item">
+            <img src="../assets/icon/016033c7ef3e0c6c.png">
+            <span>领券</span>
           </div>
-          <a href="javascript:;">更多秒杀</a>
+          <div class="list-item">
+            <img src="../assets/icon/01e0e818a7505267.png">
+            <span>京东超市</span>
+          </div>
+          <div class="list-item">
+            <img src="../assets/icon/28cda0a571b4a576.png">
+            <span>数码电器</span>
+          </div>
+          <div class="list-item">
+            <img src="../assets/icon/4792ec307a853e9f.png">
+            <span>PLUS会员</span>
+          </div>
+          <div class="list-item">
+            <img src="../assets/icon/5ae01befN2494769f.png">
+            <span>京东到家</span>
+          </div>
         </div>
-        <div class="mslist">
-          <ul>
-            <li>
-              <img src="../assets/miaosha/ms1.jpg" alt="">
-            </li>
-            <li>
-              <img src="../assets/miaosha/ms2.jpg" alt="">
-            </li>
-            <li>
-              <img src="../assets/miaosha/ms3.jpg" alt="">
-            </li>
-            <li>
-              <img src="../assets/miaosha/ms4.jpg" alt="">
-            </li>
-            <li>
-              <img src="../assets/miaosha/ms5.jpg" alt="">
-            </li>
-          </ul>
-        </div> 
+        <div class="list">
+          <div class="list-item">
+            <img src="../assets/icon/5ad87bf0N66c5db7c.png">
+            <span>京东生鲜</span>
+          </div>
+          <div class="list-item">
+            <img src="../assets/icon/5b03b74eN3541598d.png">
+            <span>充值缴费</span>
+          </div>
+          <div class="list-item">
+            <img src="../assets/icon/5b03b779N5c0b196f.png">
+            <span>赚钱</span>
+          </div>
+          <div class="list-item">
+            <img src="../assets/icon/5b03fae3N67f78fe3.png">
+            <span>9.9元拼</span>
+          </div>
+          <div class="list-item">
+            <img src="../assets/icon/65c0cdc1362635fc.png">
+            <span>京东服饰</span>
+          </div>
+        </div>
+        <!-- 下部 -->
+        <div class="zhongqiu">
+          <div>
+            <a href="javascript:;"><img src="../assets/1.jpg"></a>
+          </div>
+          <div>
+            <a href="javascript:;"><img src="../assets/2.jpg"></a>
+          </div>
+          <div>
+            <a href="javascript:;"><img src="../assets/3.jpg"></a>
+          </div>
+        </div>
+        <!-- 京东秒杀 -->
+        <div class="jdms">
+          <div>
+            <div>
+              <span></span>
+              <span>18点场</span>
+              <div class="djs">
+                <span class="number">00</span>
+                <span>:</span>
+                <span class="number">42</span>
+                <span>:</span>
+                <span class="number">50</span>
+              </div>
+            </div>
+            <a href="javascript:;">更多秒杀</a>
+          </div>
+          <div class="mslist">
+            <ul>
+              <li>
+                <img src="../assets/miaosha/ms1.jpg" alt="">
+                <span><span>￥</span>279</span>
+                <s><span>￥</span>299</s>
+              </li>
+              <li>
+                <img src="../assets/miaosha/ms2.jpg" alt="">
+                <span><span>￥</span>1279</span>
+                <s><span>￥</span>1399</s>
+              </li>
+              <li>
+                <img src="../assets/miaosha/ms3.jpg" alt="">
+                <span><span>￥</span>159</span>
+                <s><span>￥</span>129</s>
+              </li>
+              <li>
+                <img src="../assets/miaosha/ms4.jpg" alt="">
+                <span><span>￥</span>559</span>
+                <s><span>￥</span>466</s>
+              </li>
+              <li>
+                <img src="../assets/miaosha/ms5.jpg" alt="">
+                <span><span>￥</span>59</span>
+                <s><span>￥</span>100</s>
+              </li>
+              <li>
+                <img src="../assets/miaosha/ms6.png" alt="">
+                <span><span>￥</span>79</span>
+                <s><span>￥</span>89</s>
+              </li>
+              <li>
+                <img src="../assets/miaosha/ms7.jpg" alt="">
+                <span><span>￥</span>69</span>
+                <s><span>￥</span>99</s>
+              </li>
+              <li>
+                <img src="../assets/miaosha/ms8.png" alt="">
+                <span><span>￥</span>366</span>
+                <s><span>￥</span>315</s>
+              </li>
+            </ul>
+          </div> 
+        </div>
       </div>
     </div>
   </div>
@@ -117,21 +157,65 @@ export default {
 }
 </script>
 <style scoped>
+  /* app推广 */
+  .app_tg{
+    display: flex;
+    align-items: center;
+    text-align: center;
+    height: 3rem;
+  }
+  .app_tg>div{
+    height: 100%;
+    background: #232326;
+    width: 68%;
+    display: flex;
+    align-items: center;
+  }
+  .app_tg>div>img:first-child{
+    width: 1rem;
+    height: 1rem;
+    margin:1rem 1rem 1rem .5rem;
+  }
+  .app_tg>div>img:nth-child(2){
+    width: 2.2rem;
+    height: 2.2rem;
+    margin-right: .5rem;
+  }
+  .app_tg>div>div>span:first-child{
+    display: block;
+    font-size: .8rem;
+    color: #fff;
+  }
+  .app_tg>div>div>span:nth-child(2){
+    font-size: .75rem;
+    color: #999999;
+  }
+  .app_tg>div:nth-child(2){
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 32%;
+    background: #cd2525;
+  }
+  .app_tg>div:nth-child(2)>a{
+    color: #fff;
+  }
   /* 主要内容容器 */
   .container{
-    padding: 0 2.5%;
-    padding-top: 45px;
     background: url('../assets/bg.jpg') #f6f6f6 no-repeat;
     background-position: center center;
     background-size: cover;      
   }
+  .container>div:nth-child(2){
+    padding: 0 2.5%;
+  }
   /* 上方搜索框 */
   .top_input{
     padding: 12px 0;
-    width: 100%;
+    /* width: 100%;
     position: fixed;
     top:0;
-    left: 0;
+    left: 0; */
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -216,7 +300,7 @@ export default {
   }
   /* 京东秒杀 */
   .jdms{
-    padding: .5rem;
+    padding: .5rem .5rem 0 .5rem;
     border-radius: .3rem;
     background: url('../assets/miaosha/miaotiao.png') #fff no-repeat;
     background-size: 100%;
@@ -258,12 +342,32 @@ export default {
     background-size: .8rem;
     color: #e93b3d;;
   }
+  .mslist{
+    overflow-x: scroll;
+  }
+  ::-webkit-scrollbar { 
+    display: none; }
   .mslist>ul{
     display: flex;
     padding: 0;
+    width: 160%;
   }
   .mslist>ul>li{
-    width: 20%;
+    width: 13%;
+  }
+  .mslist>ul>li>span{
+    display: block;
+    font-weight: bold;
+    color: #f00;
+  }
+  .mslist>ul>li>span>span{
+    font-size: .8rem;
+    font-weight: bolder;
+    color: #f00;
+  }
+  .mslist>ul>li>s{
+    columns: #999;
+    font-size: .7rem;
   }
   .mslist>ul>li>img{
     display: block;
